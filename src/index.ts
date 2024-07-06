2aaa2edff6a672ccb6f00eed4d024a85b5bbb970
hhplum <hhplum6@gmail.com>
2024-07-06 15:45:47 +0800

    fix(index): Add log related operations

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { publish } from "./publish.ts";
 export { release } from "./release.ts";
+export {
+  changelogArgs,
+  generateChangelog,
+  getLatestTag,
+  logRecentCommits,
+} from "./utils.ts";
